8f608ba3b3ab1b066f040b57bf31132cdc61faa1
Elizaveta-may <elizavetamaistrava@gmail.com>
2025-02-17 22:12:58 +0300

    add methics

--- src/MedVisit.AccessManagement/Controllers/UserController.cs
+++ src/MedVisit.AccessManagement/Controllers/UserController.cs
@@ -4,9 +4,8 @@ using MedVisit.AccessManagement.Mediatr.User.Queries;
 using MedVisit.AccessManagement.Models.User;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using Prometheus;
 using System.ComponentModel.DataAnnotations;
-using System.Security.Claims;
 
 namespace MedVisit.AccessManagement.Controllers
 {
@@ -17,80 +16,131 @@ namespace MedVisit.AccessManagement.Controllers
     {
         private readonly IMediator _mediator;
 
+        private static readonly Counter RequestCounter = Metrics.CreateCounter("user_requests_total", "Total number of requests to user endpoints");
+        private static readonly Histogram LatencyHistogram = Metrics.CreateHistogram("user_request_latency_seconds", "Histogram of user request latencies in seconds", new HistogramConfiguration
+        {
+            Buckets = Histogram.LinearBuckets(0.01, 0.05, 10)
+        });
+        private static readonly Counter ErrorCounter = Metrics.CreateCounter("user_errors_total", "Total number of errors (500 responses)");
+
         public UserController(IMediator mediator)
         {
             _mediator = mediator;
         }
+
         [HttpGet("Healthy")]
         public IActionResult Check()
         {
+            RequestCounter.Inc();
             return Ok(new { status = "Healthy" });
         }
-        /// <summary>
-        /// Получить всех пользователей
-        /// </summary>
+
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<IEnumerable<UserDto>> GetAll()
-            => await _mediator.Send(new GetUsersQuery());
+        {
+            RequestCounter.Inc();
+            using (LatencyHistogram.NewTimer())
+            {
+                try
+                {
+                    return await _mediator.Send(new GetUsersQuery());
+                }
+                catch
+                {
+                    ErrorCounter.Inc();
+                    throw;
+                }
+            }
+        }
 
-        /// <summary>
-        /// Получить пользователя по идентификатору
-        /// </summary>
-        /// <param name="id">Идентификатор пользователя</param>
-        /// <response code="200">Возвращает найденного пользователя</response>
-        /// <response code="404">Пользователь не найден</response>
-        /// <returns>Пользователь</returns>
         [HttpGet("{id:int}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetById([Required] int id)
-            => Ok(await _mediator.Send(new GetUserByIdQuery(id)));
+        {
+            RequestCounter.Inc();
+            using (LatencyHistogram.NewTimer())
+            {
+                try
+                {
+                    var user = await _mediator.Send(new GetUserByIdQuery(id));
+                    if (user == null)
+                        return NotFound();
 
-        /// <summary>
-        /// Создать пользователя
-        /// </summary>
-        /// <param name="request">Тело запроса</param>
-        /// <response code="201">Возвращает созданного пользователя</response>
-        /// <response code="400">Входные данные некорректны</response>
-        /// <response code="409">Такой пользователь уже существует</response>
-        /// <returns>Созданный пользователь</returns>
+                    return Ok(user);
+                }
+                catch
+                {
+                    ErrorCounter.Inc();
+                    throw;
+                }
+            }
+        }
+
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status409Conflict)]
         public async Task<IActionResult> Create([FromBody] UserDto request)
         {
-            var result = await _mediator.Send(new CreateUserCommand(request));
-            return CreatedAtAction(nameof(GetById), new { id = result.Id }, result);
+            RequestCounter.Inc();
+            using (LatencyHistogram.NewTimer())
+            {
+                try
+                {
+                    var result = await _mediator.Send(new CreateUserCommand(request));
+                    return CreatedAtAction(nameof(GetById), new { id = result.Id }, result);
+                }
+                catch
+                {
+                    ErrorCounter.Inc();
+                    throw;
+                }
+            }
         }
 
-        /// <summary>
-        /// Обновить пользователя
-        /// </summary>
-        /// <param name="id">Идентификатор пользователя</param>
-        /// <param name="request">Тело запроса</param>
-        /// <response code="200">Возвращает обновленного пользователя</response>
-        /// <response code="400">Входные данные некорректны</response>
-        /// <response code="404">Пользователь не найден</response>
-        /// <returns>Пользователь</returns>
         [HttpPut("{id:int}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> Update([Required] int id, [FromBody] UserDto request)
-            => Ok(await _mediator.Send(new UpdateUserCommand(id, request)));
+        {
+            RequestCounter.Inc();
+            using (LatencyHistogram.NewTimer())
+            {
+                try
+                {
+                    var updatedUser = await _mediator.Send(new UpdateUserCommand(id, request));
+                    return Ok(updatedUser);
+                }
+                catch
+                {
+                    ErrorCounter.Inc();
+                    throw;
+                }
+            }
+        }
 
-        /// <summary>
-        /// Удалить пользователя
-        /// </summary>
-        /// <param name="id">Идентификатор пользователя</param>
-        /// <response code="200">Успех</response>
-        /// <response code="404">Пользователь не найден</response>
         [HttpDelete("{id:int}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> Delete([Required] int id)
-            => Ok(await _mediator.Send(new DeleteUserCommand(id)));
+        {
+            RequestCounter.Inc();
+            using (LatencyHistogram.NewTimer())
+            {
+                try
+                {
+                    await _mediator.Send(new DeleteUserCommand(id));
+                    return Ok();
+                }
+                catch
+                {
+                    ErrorCounter.Inc();
+                    throw;
+                }
+            }
+        }
     }
 }
